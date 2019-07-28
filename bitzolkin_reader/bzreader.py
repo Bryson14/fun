0b142a44ff9325908b654763eaba1f01b51187b7
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-07-28 15:32:58 -0600

    reading in file works

--- bitzolkin_reader/bzreader.py
+++ bitzolkin_reader/bzreader.py
@@ -1,7 +1,24 @@
 import sys
+import os
 
 
 def main(txt):
-	pass
+	for line in txt:
+		print(line)
+
 
 if __name__ == "__main__":
+	print(len(sys.argv))
+	if len(sys.argv) != 2:
+		print("ERROR\nUSAGE -- python bzreader.py TEXTFILE")
+	else:
+		try:
+			with open(sys.argv[1]) as txt:
+				main(txt)
+		except FileNotFoundError:
+			print(f"File name {sys.argv[1]} not found")
+			sys.exit(1)
+		except IOError:
+			print("something went wrong starting the program. Bye Felicia")
+			sys.exit(1)
+
