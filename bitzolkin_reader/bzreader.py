14667e23996b389e2cc3733b9ef7beff3b00ed6d
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-07-31 20:15:07 -0600

    finds btc payout correctly for each day but not the cumulitive amount

--- bitzolkin_reader/bzreader.py
+++ bitzolkin_reader/bzreader.py
@@ -2,37 +2,19 @@ import sys
 import os
 
 
-class Day(object):
-	def __init__(self, date, firstday=False):
-		self.date = date
-		self.firstday = firstday
-		if date > '2019-06-01':
-			self.bonus = True
-		else:
-			self.bonus = False
-		self.pending = 0.0
-		self.wallet = 0.0
-		self.withdraw = 0.0
-		self.funding = 0.0
-		self.notes = ''
-
-	def __str__(self):
-		return self.date + "\t" + str(self.invested) + "\t" + str(self.wallet) + "\t" + str(self.pending) + "\t" + \
-			str(self.withdraw) + "\t" + str(self.funding) + "\t" + self.notes
-
-
-def main(txt, pkgs=0):
-	last_pending = 0.0
-	last_wallet = 0.0
-	add_phrase = ['BTC Package bonus for Order', 'BTC Daily Dividend For Order']
-
-	date = ''
+def main(txt, pkgs):
+	data = {}
 	for line in reversed(list(txt)):
 		line = line.split('\t')
-		if date == line[0]:
-			if
+		if line[0] in data:
+			if 'BTC' in line[1]:
+				data[line[0]] += float(line[3])
+			elif 'Backoffice' in line[1]:
+				data[line[0]] -= float(line[3])
+		else:
+			data[line[0]] = float(line[3])
 
-
+	print(data)
 
 
 if __name__ == "__main__":
@@ -40,13 +22,16 @@ if __name__ == "__main__":
 		print("ERROR\nUSAGE -- python bzreader.py TRANSACTION_FILE PKGS_FILE")
 	else:
 		try:
+			txt = ''
+			pkgs = ''
 			with open(sys.argv[1]) as txt:
-				print("Program starting")
-				main(txt)
+				txt = txt.readlines()
+			with open(sys.argv[2]) as pkgs:
+				pkgs = pkgs.readlines()
+			main(txt, pkgs)
+
 		except FileNotFoundError:
-			print(f"File name {sys.argv[1]} not found")
+			print(f"File name {sys.argv[1]} or {sys.argv[2]} not found")
 			sys.exit(1)
-		except IOError:
-			print("something went wrong starting the program. Bye Felicia")
-			sys.exit(1)
+
 
--- bitzolkin_reader/readme.md
+++ bitzolkin_reader/readme.md
@@ -1,2 +1,4 @@
+This takes sheets of BZ tranactions lines and record them into a useful csv file. This is not useful for anyone not familiar with BZ (the discretion is intended).
+
 This will create a csv file that will import nicely into the BTC Actuals spreadsheet. Arguments are bzreader.py TRANSACTIONFILE
 For best results, paste all transactions into a notepad sheet so there is no extra formating issues.
