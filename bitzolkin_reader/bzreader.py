82b4308e1c524bbc7d04768434cf27f6c39e2740
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-08-06 15:57:53 -0600

    commit for merge to master

--- bitzolkin_reader/bzreader.py
+++ bitzolkin_reader/bzreader.py
@@ -6,19 +6,22 @@ def main(txt, pkgs):
 	data = {}
 	for line in reversed(list(txt)):
 		line = line.split('\t')
-		if line[0] in data:
+		if line[0] in data:  # adding to a existing day
 			if 'BTC' in line[1]:
 				data[line[0]] += float(line[3])
 			elif 'Backoffice' in line[1]:
 				data[line[0]] += float(line[3])
 				print(f"backoffice {float(line[3])}")
-		elif len(data) > 0:
-			#TODO fix when in the beginning there isnt a transaction for every day
-			year, month, day = line[0].split("-")
-			yesterday = (datetime.date(int(year), int(month), int(day)) - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-			print(f'line[0] {line[0]}, yesterday {yesterday}')
-			data[line[0]] = data[yesterday] + float(line[3])
-		else:
+		elif len(data) > 0:  # starting a spot in the dictionary for a new day.
+			# TODO fix when in the beginning there isnt a transaction for every day
+			try:
+				year, month, day = line[0].split("-")
+				yesterday = (datetime.date(int(year), int(month), int(day)) - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+				# print(f'line[0] {line[0]}, yesterday {yesterday}')
+				data[line[0]] = data[yesterday] + float(line[3])
+			except KeyError:
+				pass
+		else:  # when adding the first day in the dictionary
 			data[line[0]] = float(line[3])
 
 	print(data)
