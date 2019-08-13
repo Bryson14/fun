c75daea78de6f2d5b416c0a9cbe92cd53b6b5ca9
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-08-13 13:40:20 -0600

    recursion is hard lol

--- N-Bonacci_Numbers/n_bonacci_numbers.py
+++ N-Bonacci_Numbers/n_bonacci_numbers.py
@@ -1,7 +1,19 @@
 import sys
 
 
-def bonacci_sequence(sum_length, n):
+def bonacci_sequence_recur(sum_length, n, seq=[]):
+	if len(seq) > n:
+		print("sequence: ", seq)
+	elif n < 0:
+		print("Not able to find a negative Nth digit in Bonacci sequence.")
+		sys.exit(1)
+	elif sum_length < 2:
+		print(f"Not able to create a sequence with a sum length of {sum_length}")
+		sys.exit(1)
+	else:
+		pass
+
+def bonacci_sequence_iter(sum_length, n):
 
 	if sum_length < 2:
 		print(f"Not able to create a sequence with a sum length of {sum_length}")
@@ -9,7 +21,7 @@ def bonacci_sequence(sum_length, n):
 
 	elif n < 0:
 		print("Not able to find a negative Nth digit in Bonacci sequence.")
-		sys.argv(1)
+		sys.exit(1)
 
 	else:
 		# starts the list with correct amounts of 0's and 1 at the end
@@ -28,11 +40,23 @@ def bonacci_sequence(sum_length, n):
 
 		print(start)
 
+
 if __name__ == "__main__":
 
-	if len(sys.argv) != 3:
-		print("Usage n_bonacci_numbers.py LENGTH_OF_SUM Nth_NUMBER")
+	if len(sys.argv) < 3 or len(sys.argv) > 4:
+		print("Usage n_bonacci_numbers.py [-r][-i] LENGTH_OF_SUM Nth_NUMBER"
+		      "-r means recursion, -i iteration. default is iteration")
 		sys.exit(1)
 
+	elif len(sys.argv) == 3:
+		bonacci_sequence_iter(int(sys.argv[1]), int(sys.argv[2]))
+		sys.exit(1)
+	elif sys.argv[1] == '-i':
+		bonacci_sequence_iter(int(sys.argv[2]), int(sys.argv[3]))
+		sys.exit(1)
+	elif sys.argv[1] == '-r':
+		bonacci_sequence_recur(int(sys.argv[2]), int(sys.argv[3]))
+		sys.exit(1)
 	else:
-		bonacci_sequence(int(sys.argv[1]), int(sys.argv[2]))
+		print("unknown command")
+		sys.exit(1)
